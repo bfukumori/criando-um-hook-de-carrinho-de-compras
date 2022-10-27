14df3b110c07ec0d02f676c3bb2a553e4d88a7bd
Bruno Fukumori <brunofukumori@gmail.com>
2022-10-27 03:04:30 -0300

    change: heroku to my json server

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -19,32 +19,31 @@ interface Product {
 const Cart = (): JSX.Element => {
   const { cart, removeProduct, updateProductAmount } = useCart();
 
-  const cartFormatted = cart.map(product => {
+  const cartFormatted = cart.map((product) => {
     return {
       ...product,
       priceFormatted: formatPrice(product.price),
-      subTotal: formatPrice(product.amount * product.price)
-    }
-  })
+      subTotal: formatPrice(product.amount * product.price),
+    };
+  });
 
-  const total =
-    formatPrice(
-      cart.reduce((sumTotal, product) => {
-        return sumTotal + (product.price * product.amount);
-      }, 0)
-    )
+  const total = formatPrice(
+    cart.reduce((sumTotal, product) => {
+      return sumTotal + product.price * product.amount;
+    }, 0)
+  );
 
   function handleProductIncrement(product: Product) {
     updateProductAmount({
       productId: product.id,
-      amount: product.amount + 1
+      amount: product.amount + 1,
     });
   }
 
   function handleProductDecrement(product: Product) {
     updateProductAmount({
       productId: product.id,
-      amount: product.amount - 1
+      amount: product.amount - 1,
     });
   }
 
@@ -52,22 +51,25 @@ const Cart = (): JSX.Element => {
     removeProduct(productId);
   }
 
+  function handleFinishPayment() {
+    console.log(cart);
+  }
+
   return (
     <Container>
       <ProductTable>
         <thead>
           <tr>
-            <th aria-label="product image" />
+            <th aria-label='product image' />
             <th>PRODUTO</th>
             <th>QTD</th>
             <th>SUBTOTAL</th>
-            <th aria-label="delete icon" />
+            <th aria-label='delete icon' />
           </tr>
         </thead>
         <tbody>
-          {cartFormatted.map(product => (
-            <tr data-testid="product"
-              key={product.id}>
+          {cartFormatted.map((product) => (
+            <tr data-testid='product' key={product.id}>
               <td>
                 <img src={product.image} alt={product.title} />
               </td>
@@ -78,22 +80,22 @@ const Cart = (): JSX.Element => {
               <td>
                 <div>
                   <button
-                    type="button"
-                    data-testid="decrement-product"
+                    type='button'
+                    data-testid='decrement-product'
                     disabled={product.amount <= 1}
                     onClick={() => handleProductDecrement(product)}
                   >
                     <MdRemoveCircleOutline size={20} />
                   </button>
                   <input
-                    type="text"
-                    data-testid="product-amount"
+                    type='text'
+                    data-testid='product-amount'
                     readOnly
                     value={product.amount}
                   />
                   <button
-                    type="button"
-                    data-testid="increment-product"
+                    type='button'
+                    data-testid='increment-product'
                     onClick={() => handleProductIncrement(product)}
                   >
                     <MdAddCircleOutline size={20} />
@@ -105,8 +107,8 @@ const Cart = (): JSX.Element => {
               </td>
               <td>
                 <button
-                  type="button"
-                  data-testid="remove-product"
+                  type='button'
+                  data-testid='remove-product'
                   onClick={() => handleRemoveProduct(product.id)}
                 >
                   <MdDelete size={20} />
@@ -118,7 +120,9 @@ const Cart = (): JSX.Element => {
       </ProductTable>
 
       <footer>
-        <button type="button">Finalizar pedido</button>
+        <button type='button' onClick={handleFinishPayment}>
+          Finalizar pedido
+        </button>
 
         <Total>
           <span>TOTAL</span>
